e4d1b97ed2ecec62867974377a82f6aa2d65d829
Ash_win <62272001+ash-win-cs@users.noreply.github.com>
2020-04-03 15:50:13 +0530

    update database

--- database.go
+++ database.go
@@ -10,16 +10,16 @@ import (
 
 func main() {
 	database, _ := sql.Open("sqlite3", "./nraboy.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (adno INTEGER PRIMARY KEY, firstname TEXT, semester TEXT)")
 	statement.Exec()
-	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
-	statement.Exec("Nic", "Raboy")
-	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+	statement, _ = database.Prepare("INSERT INTO people (adno, firstname, semester) VALUES (?, ?, ?)")
+	statement.Exec(170120, "ashwin", "s6")
+	rows, _ := database.Query("SELECT adno, firstname, semester FROM people")
 	var id int
 	var firstname string
-	var lastname string
+	var semester string
 	for rows.Next() {
-		rows.Scan(&id, &firstname, &lastname)
+		rows.Scan(&id, &firstname, &semester)
 		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
 	}
 }
