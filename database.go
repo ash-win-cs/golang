407f22bdbc3073ab82a9815e00df45fec164b594
Ash_win <62272001+ash-win-cs@users.noreply.github.com>
2020-04-04 15:25:56 +0530

    updated with sql time function

--- database.go
+++ database.go
@@ -11,21 +11,34 @@ func adduser(Rno string, name string) {
 	database, _ := sql.Open("sqlite3", "./nraboy.db")
 	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS register (RNo TEXT, Name TEXT, date TEXT)")
 	statement.Exec()
-	statement, _ = database.Prepare("INSERT INTO register (Rno, Name, date) VALUES (?, ?, ?)")
+	rows, _ := database.Query("SELECT datetime('now') as 'Current date & time';")
 	var time string
+	rows.Scan(&time)
+	statement, _ = database.Prepare("INSERT INTO register (Rno, Name, date) VALUES (?, ?, ?)")
 	statement.Exec(Rno, name, time)
 
 }
 
-func checkfield(number string) {
+func checkfield(number string, uname string) int {
+	var status int = 0
 	database, _ := sql.Open("sqlite3", "./nraboy.db")
 	rows, _ := database.Query("SELECT Name FROM database WHERE Rno=\"%s\"", number)
 	var Name string
 	rows.Scan(&Name)
-	fmt.Println(Name)
+	if uname == Name {
+		adduser(number, Name)
+		status = 1
+	}
 
+	return (status)
 }
 
 func main() {
+
+	if checkfield("NSS17EC036", "Ashwin C S") == 0 {
+		fmt.Println("No Users Found!!")
+	} else {
+		fmt.Println("welcome")
+	}
 
 }
