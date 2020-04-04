ba88f4143e0201ea52eaaf9d4ad217c3c4bfebab
Ash_win <62272001+ash-win-cs@users.noreply.github.com>
2020-04-04 14:20:27 +0530

    added new function

--- database.go
+++ database.go
@@ -3,23 +3,26 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
+func adduser() {
 	database, _ := sql.Open("sqlite3", "./nraboy.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (adno INTEGER PRIMARY KEY, firstname TEXT, semester TEXT)")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS register (RNo TEXT, Name TEXT, date TEXT)")
 	statement.Exec()
-	statement, _ = database.Prepare("INSERT INTO people (adno, firstname, semester) VALUES (?, ?, ?)")
-	statement.Exec(170120, "ashwin", "s6")
-	rows, _ := database.Query("SELECT adno, firstname, semester FROM people")
-	var id int
-	var firstname string
-	var semester string
-	for rows.Next() {
-		rows.Scan(&id, &firstname, &semester)
-		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
-	}
+
+}
+
+func checkfield(number string) {
+	database, _ := sql.Open("sqlite3", "./nraboy.db")
+	rows, _ := database.Query("SELECT Name FROM database WHERE Rno=\"%s\"", number)
+	var Name string
+	rows.Scan(&Name)
+	fmt.Println(Name)
+
+}
+
+func main() {
+
 }
